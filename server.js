fcf6156fae132ed6ad8ba9f064a8a17c24f0465b
Philippe Dixon <pdixon555@hotmail.com>
2017-11-09 13:31:18 -0500

    debug deployment

--- server.js
+++ server.js
@@ -12,10 +12,11 @@ app.use(express.static("client/build"));
 
 app.use(routes);
 
+
 mongoose.Promise = global.Promise;
 
-mongoose.connect(
-  process.env.MONGODB_URI || "mongodb://heroku_tkw4flts:unpbbcekr66n2is7caou7fnuta@ds255265.mlab.com:55265/heroku_tkw4flts", 
+mongoose.connect("mongodb://heroku_tkw4flts:unpbbcekr66n2is7caou7fnuta@ds255265.mlab.com:55265/heroku_tkw4flts",
+  //process.env.MONGODB_URI || "mongodb://localhost/nytimesReact" || "mongodb://heroku_tkw4flts:unpbbcekr66n2is7caou7fnuta@ds255265.mlab.com:55265/heroku_tkw4flts", 
   {
     useMongoClient: true
   }
@@ -24,5 +25,3 @@ mongoose.connect(
 app.listen(PORT, function() {
   console.log(`🌎  ==> API Server now listening on PORT ${PORT}!`);
 });
-
-//
